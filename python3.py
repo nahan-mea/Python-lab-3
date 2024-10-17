28aa604aa24ed5032e4a591da39409e623908d60
nahan-mea <24MCAG07@meaec.edu.in>
2024-10-17 09:04:42 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,8 @@
+def sum_of_list(lst):
+    total = 0
+    for item in lst:
+        total += item
+    return total
+
+numbers = [1, 2, 3, 4, 5]
+print(f"Sum of the list is: {sum_of_list(numbers)}")
---
+++ python5.py
@@ -0,0 +1,4 @@
+n = int(input("Enter a number: "))
+print(f"Multiplication table of {n}:")
+for i in range(1, 11):
+    print(f"{n} x {i} = {n * i}")
